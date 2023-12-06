e0d7588d73f778a0e0e38d1f5991a856597991c1
dna737 <adnanhere27@gmail.com>
2023-12-05 22:11:39 -0600

    try to insert valid stocks

--- src/scraper-for-db/parser.py
+++ src/scraper-for-db/parser.py
@@ -1,6 +1,11 @@
+import uuid
+from dotenv import load_dotenv
+import os
 import requests
 from bs4 import BeautifulSoup
+from supabase import create_client, Client
 
+load_dotenv()
 website_url = "https://www.morningstar.com/stocks"
 
 r = requests.get(website_url)
@@ -17,5 +22,21 @@ for ul in sth.find_all("ul"):
         names.append(currStr.strip()[0 : currStr.index("(")])
         symbols.append(currStr.strip()[currStr.index("(") : len(currStr.strip())])
 
-print("names:", len(names))
-print("symbols:", len(symbols))
+data = []
+
+url: str = os.environ.get("VITE_SUPABASE_CLIENT_URL")
+key: str = os.environ.get("VITE_SUPABASE_CLIENT_PW")
+supabase: Client = create_client(url, key)
+
+if url is None:
+    print("oop")
+
+# inserting the data:
+for i in range(len(names)):
+    uuid_int = int.from_bytes(uuid.uuid4().bytes, byteorder="big", signed=False)
+    uuid_int_within_range = uuid_int % (2**63 - 1) - (2**63 - 1)
+    sth, count = (
+        supabase.table("ValidStocks")
+        .insert({"id": uuid_int_within_range, "Ticker": symbols[i], "Name": names[i]})
+        .execute()
+    )
