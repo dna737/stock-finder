acc773c65b1f1027fab03c2766413f7991e9d83c
dna737 <adnanhere27@gmail.com>
2023-12-07 20:01:38 -0600

    write db tables to a file

--- src/scraper-for-db/parser.py
+++ src/scraper-for-db/parser.py
@@ -1,9 +1,7 @@
 import uuid
 from dotenv import load_dotenv
-import os
 import requests
 from bs4 import BeautifulSoup
-from supabase import create_client, Client
 
 load_dotenv()
 website_url = "https://www.morningstar.com/stocks"
@@ -19,24 +17,21 @@ sth = soup.find("div", attrs={"class": "mdc-carousel__inner"})
 for ul in sth.find_all("ul"):
     for span in ul.find_all("span"):
         currStr = span.get_text().replace("\n", "").replace("\t", "")
-        names.append(currStr.strip()[0 : currStr.index("(")])
+        names.append(currStr.strip()[0 : currStr.index("(") - 1])
         symbols.append(currStr.strip()[currStr.index("(") : len(currStr.strip())])
 
 data = []
+print(symbols)
 
-url: str = os.environ.get("VITE_SUPABASE_CLIENT_URL")
-key: str = os.environ.get("VITE_SUPABASE_CLIENT_PW")
-supabase: Client = create_client(url, key)
-
-if url is None:
-    print("oop")
-
-# inserting the data:
+f = open("../database_info.txt", "a")
 for i in range(len(names)):
-    uuid_int = int.from_bytes(uuid.uuid4().bytes, byteorder="big", signed=False)
-    uuid_int_within_range = uuid_int % (2**63 - 1) - (2**63 - 1)
-    sth, count = (
-        supabase.table("ValidStocks")
-        .insert({"id": uuid_int_within_range, "Ticker": symbols[i], "Name": names[i]})
-        .execute()
+    f.write(
+        str(uuid.uuid4())
+        + ","
+        + names[i].strip().replace(",", "")
+        + ","
+        + symbols[i].strip().replace(",", "").replace("(", "").replace(")", "")
+        + "\n"
     )
+
+f.close()
