23b8a0b2b947144763183f7a98d93b32736f3b0f
dna737 <adnanhere27@gmail.com>
2023-12-23 11:31:27 -0600

    add buyMeACoffee button

--- src/auth.jsx
+++ src/auth.jsx
@@ -1,3 +1,4 @@
+import { BuyMeACoffee } from "./Coffee";
 import InputForm from "./InputForm";
 import "./index.css";
 import { useState, useEffect } from "react";
@@ -61,10 +62,47 @@ export default function App() {
         return (
             <div className="flex flex-col my-3.5 mx-3.5 justify-between w-full h-full">
                 <InputForm onSubmit={handleSubmit} />
-                {(currInput[1] && <Graph ticker={currInput[0]} />) || (
-                    <h1 className="text-white">WOW</h1>
-                )}
+                {(currInput[1] && <Graph ticker={currInput[0]} />) ||
+                    (<ErrorComponent /> && activateCoffeeButton())}
             </div>
         );
     }
+}
+
+function activateCoffeeButton() {
+    document.getElementById("buyMeACoffee").showModal();
+}
+
+function ErrorComponent() {
+    return (
+        <div className="flex flex-col justify-center my-3.5 mx-3.5 w-full h-full">
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <button
+                className="btn hidden"
+                onClick={() =>
+                    document.getElementById("buyMeACoffee").showModal()
+                }
+            ></button>
+            <dialog id="my_modal_4" className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
+                    <h3 className="font-bold text-lg">
+                        API credits used up for the minute!
+                    </h3>
+                    <p className="py-4">
+                        The Graph could not be generated due to the free-tier
+                        API plan this project is currently using. If you wish to
+                        get rid of rate-limits, considering buying me a coffee.
+                        ;&rpar;
+                    </p>
+                    <div className="modal-action">
+                        <form method="dialog" className="flex-col">
+                            {/* if there is a button, it will close the modal */}
+                            <BuyMeACoffee />
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
 }
